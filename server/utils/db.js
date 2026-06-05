a8df8f4381bb4d7f46bcbb7137e26efa1933a9db
EDZ <dushyantkuverma2001@gmail.com>
2026-06-05 19:45:59 +0530

    Your descriptive commit message

--- server/utils/db.js
+++ server/utils/db.js
@@ -5,25 +5,14 @@ const { PrismaPg } = require("@prisma/adapter-pg");
 const globalForPrisma = globalThis;
 
 const prismaClientSingleton = () => {
-    const pool = new Pool({
-        connectionString: process.env.DATABASE_URL,
-    });
-
+    const connectionString = process.env.DATABASE_URL;
+    const pool = new Pool({ connectionString });
     const adapter = new PrismaPg(pool);
 
     return new PrismaClient({
-        adapter,
-        log:
-            process.env.NODE_ENV === "development"
-                ? ["query", "info", "warn", "error"]
-                : ["error", "warn"],
+        adapter, // This handles the database connection now
     });
 };
 
 const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
-
-if (process.env.NODE_ENV !== "production") {
-    globalForPrisma.prisma = prisma;
-}
-
 module.exports = prisma;
