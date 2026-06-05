8db333bc309ffa3549d1b18176487dd6faffeee9
EDZ <dushyantkuverma2001@gmail.com>
2026-06-05 10:42:19 +0530

    Your descriptive commit message

--- server/app.js
+++ server/app.js
@@ -167,7 +167,7 @@ app.use((err, req, res, next) => {
   handleServerError(err, res, `${req.method} ${req.path}`);
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
   console.log('Rate limiting and request logging enabled for all endpoints');
